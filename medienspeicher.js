f50724b8d6acbd27aef7cc7ed6c32a2db5a8be82
BenjaminBleske <136079434+BenjaminBleske@users.noreply.github.com>
2026-06-10 14:31:45 +0200

    Add files via upload

---
+++ medienspeicher.js
@@ -0,0 +1,139 @@
+var HINWEIS_EXTERN = "Hinweis zu externen Inhalten:\nÜber die nachfolgende Schaltfläche verlassen Sie den Internetauftritt des Projektblogs von EKLIPS3plus1. Die hier aufgeführten Verweise auf externe Links dienen ausschließlich der sachlichen Aggregation von Inhalten der Partner des hiesigen Verbundvorhabens sowie vereinzelten öffentlich-rechtlichen Beiträgen zum Medienecho zu EKLIPS3plus1.\nAuf die Inhalte, die Gestaltung und die Verfügbarkeit der externen Links hat dieser Projektblog keinen Einfluss. Die Verantwortung für diese Inhalte liegt allein bei den jeweiligen Anbietern. Zum Zeitpunkt der Verlinkung wurden die externen Seiten auf mögliche Rechtsverstöße geprüft; rechtswidrige Inhalte waren zu diesem Zeitpunkt nicht erkennbar. Eine ständige inhaltliche Kontrolle der verlinkten Seiten ist ohne konkrete Anhaltspunkte einer Rechtsverletzung für den hiesigen Projektblog nicht zumutbar. Sollten Sie auf Rechtsverletzungen aufmerksam werden, bitten wir um einen entsprechenden Hinweis, damit wir den betreffenden Link unverzüglich entfernen können."
+
+var MEDIENSPEICHER = [
+  {
+    "id": 1,
+    "datum": "15. April 2026 ",
+    "quelle": "Stadt Wuppertal",
+    "titel": "Pressemeldung: Bergische Städte und Wupperverband starten Förderprojekt EKLIPS 3+1 | Wuppertal",
+    "text": "Wie können Städte schneller und effizienter auf den Klimawandel reagieren? Wie lassen sich Planungsprozesse beschleunigen, um Hitze, Starkregen oder Überflutungen besser zu begegnen? Die Städte Remscheid, Solingen und Wuppertal arbeiten gemeinsam mit dem Wupperverband an einem neuen Projekt namens EKLIPS3+1.",
+    "link": "https://www.wuppertal.de/presse/meldungen/meldungen-2026/april/eklips3-1.php",
+    "bild": "https://www.wuppertal.de/presse/meldungen/meldungen-2026/april/eklips3-1.php.media/582433/SvS_-3735.jpg.scaled/7a49950de6ae8ba43e6adbb1ff8f8c95.jpg",
+    "bildCaption": "Ingo Noppen, Miriam Scherff, Sven Wolf, Dr. Christine Pohl, Daniel Flemm",
+    "zitate": [
+      {
+        "text": "Als Wupperverband arbeiten wir bereits im gesamten Flusseinzugsgebiet der Wupper mit den Kommunen in der Klimafolgenanpassung zusammen. Durch EKLIPS3+1 wird nun die gemeinsame Nutzung von Urbanen Zwillingen als innovative Planungsgrundlage weiter vorangetrieben.",
+        "person": "Ingo Noppen, Vorstand des Wupperverbands"
+      },
+      {
+        "text": "Digitalisierung betrifft uns alle – die bergischen Synergien wollen und werden wir nun nutzen. Bei EKLIPS3+1 kommen modernste Technologien wie Künstliche Intelligenz und Big Data zum Einsatz. Ob aktuelle Klima-, Geo- oder Fernerkundungsdaten – wir schaffen eine gemeinsame Faktenbasis, mit der wir viel effizienter steuern können. So werden wir als bergische Kommunen anpassungsfähiger, schneller und zielgenauer",
+        "person": "Daniel Flemm, Oberbürgermeister Solingen"
+      },
+      {
+        "text": "Die Lösungen, die durch unsere  interkommunale Zusammenarbeit im Förderprojekt EKLIPS3+1 entstehen, sind wiederverwendbare Werkzeuge, die auch auf andere Kommunen übertragbar sind. Wir wollen damit zu Vorreitern für den Einsatz digitaler Technik in Planungsprozessen und besonders in Fragen der Klimaresilienz werden.",
+        "person": "Sveb Wolf, Oberbürgermeister Remscheid"
+      },
+      {
+        "text": "Planungsprozesse zu beschleunigen, ist eines unserer wichtigsten Ziele überhaupt“, betont Wuppertals Oberbürgermeisterin Miriam Scherff. „Nicht nur um der Herausforderung Klimawandel besser zu begegnen, sondern für die Stadtentwicklung insgesamt. Wir werden dies nun im bergischen Städtedreieck in einem gemeinsamen Förderprojekt voranbringen und uns damit als digitale Modellregion weiter profilieren.",
+        "person": "Miriam Scherff, oberbürgermeisterin Wuppertal"
+      }
+    ]
+  },
+
+  {
+    "id": 2,
+    "datum": "19. Mai 2026 (automatisch belegt)",
+    "quelle": "Wupperverband",
+    "titel": "[externer Link] Projektseite des Wupperverbands zu EKLIPS3plus1",
+    "text": HINWEIS_EXTERN,
+    "link": "https://www.wupperverband.de/projekte/forschung/2025/10/eklips3plus1"
+  },
+
+  {
+    "id": 3,
+    "datum": "19. Mai 2026 (automatisch belegt)",
+    "quelle": "TU Dortmund",
+    "titel": "[externer Link] Projektseite der Fakultät Raumplanung, Fachgebiet Stadt- und Regionalplanung der TU-Dortmund zu EKLIPS3plus1",
+    "text": HINWEIS_EXTERN,
+    "link": "https://srp.raumplanung.tu-dortmund.de/forschung/aktuelle-projekte/eklips3plus1/"
+  },
+
+  {
+    "id": 4,
+    "datum": "15. April 2026, 19:52 Uhr",
+    "quelle": " WDR Fernsehen, Lokalzeit Bergisches Land",
+    "titel": "[externer Link] Bergische Großstädte wollen digitale Modellregion werden",
+    "text": HINWEIS_EXTERN,
+    "link": "https://www1.wdr.de/nrw/bergisches-land/wuppertal-solingen-remscheid-hochwasser-klimaschutz-projekt-100.html"
+  },
+
+  {
+    "id": 5,
+    "datum": "23. April 2026",
+    "quelle": "FONA: Forschungs für Nachhaltigkeit",
+    "titel": "[externer Link] FONA-Fördermaßnahmen Seite zu EKLIPS3plus1 ",
+    "text": HINWEIS_EXTERN,
+    "link": "https://www.fona.de/de/massnahmen/foerdermassnahmen/UDZ-Klimaanpassung/EKLIPS3plus1.php"
+  },
+
+  {
+    "id": 6,
+    "datum": "19. Mai 2026 (automatisch belegt)",
+    "quelle": "Universität Potsdam",
+    "titel": "[externer Link] Projektseite der Arbeitsgruppe Geographie und Naturrisikenforschung des Instituts für Umweltwissenschaften und Geograpghie der Universität Potsdam",
+    "text": HINWEIS_EXTERN,
+    "link": "https://www.uni-potsdam.de/de/umwelt/forschung/ag-geographie-und-naturrisikenforschung/forschungsprojekte/kommunalbefragung-1-1"
+  },
+
+  {
+    "id": 9,
+    "datum": "19. Mai 2026 (automatisch belegt)",
+    "quelle": "DFKI",
+    "titel": "[externer Link] Projektseite des DFKI zum Verbundvorhaben EKLIPS3plus1",
+    "text": HINWEIS_EXTERN,
+    "link": "https://www.dfki.de/en/web/research/projects-and-publications/project/eklips3plus1"
+  },
+
+  {
+    "id": 10,
+    "datum": "19. Mai 2026 (automatisch belegt)",
+    "quelle": "HafenCity Universität Hamburg",
+    "titel": "[externer Link] Projektseite des Fachbgebiets Umweltgerechte Stadt- und Infrastrukturplanung (USIP) der HafenCity University Hamburg zum Verbundvorhaben EKLIPS3plus1",
+    "text": HINWEIS_EXTERN,
+    "link": "https://forschungsportal.hcu-hamburg.de/en/projects/verbundprojekt-planungsbeschleunigung-effizient-und-klimaresilien/"
+  },
+
+  {
+    "id": 12,
+    "datum": "15. April 2026",
+    "quelle": "Wupperverband",
+    "titel": "[externer Link] Pressemeldung: Bergische Städte und Wupperverband starten Förderprojekt EKLIPS 3+1  | Wupperverband",
+    "text": HINWEIS_EXTERN,
+    "link": "https://www.wupperverband.de/meldung/pressemitteilungen/2026/04/bergische-staedte-und-wupperverband-starten-foerderprojekt-eklips-31"
+  },
+
+  {
+    "id": 13,
+    "datum": "19. Mai 2026 (automatisch belegt)",
+    "quelle": "smart.wuppertal",
+    "titel": "Bergische Städte und Wupperverband starten Förderprojekt EKLIPS 3+1 | smart.wuppertal",
+    "text": "Wie können Städte schneller und effizienter auf den Klimawandel reagieren? Wie lassen sich Planungsprozesse beschleunigen, um Hitze, Starkregen oder Überflutungen besser zu begegnen? Die Städte Remscheid, Solingen und Wuppertal arbeiten gemeinsam mit dem Wupperverband an einem neuen Projekt namens EKLIPS3+1.",
+    "link": "https://smart.wuppertal.de/beteiligung/eklips.php",
+    "bild": "https://smart.wuppertal.de/aktuelles/bilder-aktuelles/veranstaltungen/SvS_-3771.jpg.scaled/6ec5d3097537343611d13923580d6d24.jpg",
+    "bildCaption": "Dr. Christine Pohl",
+    "zitate": [
+      {
+        "text": "Klimawandel macht nicht an Stadtgrenzen halt. Deshalb brauchen wir gemeinsame Lösungen, die weitergehen. Das neue Förderprojekt EKLIPS3+1 ist ein wichtiger Schritt, um uns noch stärker als bisher zu vernetzen und den gemeinsamen „Datenschatz“ für konkrete Maßnahmen in unseren Städten zu nutzen.",
+        "person": "Dr. Christine Pohl, Fachliche Leitung DigiTal Zwilling"
+      }
+    ]
+  },
+
+
+  {
+    "id": 16,
+    "datum": "15. April 2026 (automatisch belegt)",
+    "quelle": "Stadt Solingen",
+    "titel": "Pressemitteilung Nr. 149: Neues Förderprojekt EKLIPS3plus1",
+    "text": "Hinweis zu externen Inhalten\nÜber die nachfolgende Schaltfläche verlassen Sie den Internetauftritt der Stadt Wuppertal. Die hier aufgeführten Verweise auf externe Presseerzeugnisse dienen ausschließlich der sachlichen Dokumentation des Medienechos zum Verbundvorhaben „EKLIPS3plus1“.\nAuf die Inhalte, die Gestaltung und die Verfügbarkeit der verlinkten externen Seiten hat die Stadt Wuppertal keinen Einfluss. Die Verantwortung für diese Inhalte liegt allein bei den jeweiligen Anbietern. Zum Zeitpunkt der Verlinkung wurden die externen Seiten auf mögliche Rechtsverstöße geprüft; rechtswidrige Inhalte waren zu diesem Zeitpunkt nicht erkennbar. Eine ständige inhaltliche Kontrolle der verlinkten Seiten ist ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Sollten Sie auf Rechtsverletzungen aufmerksam werden, bitten wir um einen entsprechenden Hinweis, damit wir den betreffenden Link unverzüglich entfernen können.",
+    "link": "https://solingen.de/inhalt/verzeichnis/pressrelease/8977",
+    "zitate": [
+      {
+        "text": "Digitalisierung betrifft uns alle – die bergischen Synergien wollen und werden wir nun nutzen. Bei EKLIPS3+1 kommen modernste Technologien wie Künstliche Intelligenz und Big Data zum Einsatz. Ob aktuelle Klima-, Geo- oder Fernerkundungsdaten – wir schaffen eine gemeinsame Faktenbasis, mit der wir viel effizienter steuern können. So werden wir als bergische Kommunen anpassungsfähiger, schneller und zielgenauer.",
+        "person": "Daniel Flemm, Oberbürgermeister Solingen"
+      }
+    ]
+  }
+]; // <- nicht loeschen
+
+var ANGEPINNT_IDS = [1, 4]; // IDs der angepinnten Beitraege
